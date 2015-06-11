c5d7045ed3b85648475cb880058aa40bc23a5c6e
seaders <seaders69@gmail.com>
2015-06-11 18:47:38 +0100

    Initial commit

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+using Amazon.CognitoIdentity;
+using Amazon.Runtime;
+using System;
+using Amazon;
+using Amazon.SecurityToken;
+using System.Collections.Generic;
+using Amazon.CognitoSync.SyncManager;
+using Amazon.CognitoSync;
+using System.Threading;
+
+public class Main : MonoBehaviour
+{
+	public static readonly RegionEndpoint COGNITO_REGION = RegionEndpoint.USEast1;
+
+	private CognitoAWSCredentials _credentials;
+	public CognitoAWSCredentials credentials { get { return IfNull(
+		ref this._credentials,
+		() => new CognitoAWSCredentials(
+			identityPoolId: _IdentityPoolId,
+			cibClient: new AmazonCognitoIdentityClient(
+				new AnonymousAWSCredentials(),
+				new AmazonCognitoIdentityConfig { Timeout = TimeSpan.FromSeconds(5), RegionEndpoint = COGNITO_REGION }
+			),
+			stsClient: new AmazonSecurityTokenServiceClient(
+				new AnonymousAWSCredentials(),
+				new AmazonSecurityTokenServiceConfig { Timeout = TimeSpan.FromSeconds(5), RegionEndpoint =  COGNITO_REGION }
+			),
+			accountId: null, unAuthRoleArn: null, authRoleArn: null
+		)
+	); } }
+
+	private CognitoSyncManager _syncManager;
+	public CognitoSyncManager syncManager { get { return IfNull(
+		ref this._syncManager,
+		() => new CognitoSyncManager(
+			credentials,
+			new AmazonCognitoSyncConfig { Timeout = TimeSpan.FromSeconds(5), RegionEndpoint = COGNITO_REGION }
+		)
+	); } }
+
+	private Dataset _dataset;
+	public Dataset dataset { get { return IfNull(
+		ref this._dataset,
+		() => syncManager.OpenOrCreateDataset("myDataset")
+	); } }
+	
+	public string _IdentityPoolId;
+	private bool _IsSetup = false;
+	
+	public static T IfNull<T>(ref T value, Func<T> lambda, bool notDefault=false)
+	{
+		if( (null == value) || ( notDefault && EqualityComparer<T>.Default.Equals(default(T), value) ) )
+		{
+			value = lambda.Invoke();
+		}
+		return value;
+	}
+
+	void Start ()
+	{
+		credentials.GetCredentialsAsync( result => {
+			if(null != result.Exception)
+			{
+				Debug.Log(result.Exception);
+				return;
+			}
+			Debug.Log(result.Response);
+			Debug.Log( credentials.GetIdentityId() );
+			_IsSetup = true;
+		} );
+	}
+	
+	
+	public void OnApplicationPause(bool isSuspended)
+	{
+		if(!_IsSetup)
+		{
+			return;
+		}
+
+		if(!isSuspended)
+		{
+			ManualResetEvent waitLock = new ManualResetEvent(false);
+			
+			EventHandler<SyncSuccessEvent> afterSync = null;
+			EventHandler<SyncFailureEvent> afterFail = null;
+			
+			Action unsubscribe = () => {
+				Debug.Log("Returned");
+				waitLock.Set();
+				dataset.OnSyncSuccess -= afterSync;
+				dataset.OnSyncFailure -= afterFail;
+			};
+			
+			afterSync = (object sender, SyncSuccessEvent e) => {
+				unsubscribe.Invoke();
+			};
+			afterFail = (object sender, SyncFailureEvent e) => {
+				unsubscribe.Invoke();
+			};
+			
+			dataset.OnSyncSuccess += afterSync;
+			dataset.OnSyncFailure += afterFail;
+			Debug.Log("Synchronizing");
+			dataset.Synchronize();
+			
+			Debug.Log("Waiting");
+//			waitLock.WaitOne();
+		}
+	}
+}
